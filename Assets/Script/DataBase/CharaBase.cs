85c45eef1c281ccaea982424fcbdb2ad82e0b52b
Shaf <KDT24SGC40005@edu.kobe-tech.ac.jp>
2025-12-28 17:42:01 +0900

    どうしようかな　いったんステータス色々突っ込んだ

--- Assets/Script/DataBase/CharaBase.cs
+++ Assets/Script/DataBase/CharaBase.cs
@@ -4,6 +4,11 @@ namespace Data
 {
     public class CharaBase
     {
-        
+        // 基本ステータスを入れる用
+        private Data.StatusBase Status;
+
+
+        // 基本ステータスの取得
+        public Data.StatusBase GetStatus() { return Status; }
     }
 }
--- Assets/Script/DataBase/CharaStatus.cs
+++ Assets/Script/DataBase/CharaStatus.cs
@@ -1,28 +1,59 @@
+using JetBrains.Annotations;
 using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Data
 {
-    // 属性
+    /// <summary>
+    /// 属性 0:水 1:炎 2:花
+    /// </summary>
     public enum ElementType
     {
         Water,  // 水
         Fire,   // 炎
         Flower  // 花
     }
-    // キャラクター全般のステータス
+    /// <summary>
+    /// キャラクター全般の基本ステータス
+    /// レベル１時点での基礎値
+    /// </summary>
     public class StatusBase
     {
-        public int id;
-        public string name;
+        public int id;      // ID
+        public string name; // 名前
 
+        public int baseAtk;     // 攻撃力
+        public int baseDef;     // 防御力
+        public int baseMatk;    // 魔法攻撃力
+        public int baseMdef;    // 魔法防御力
+        public int baseMp;      // 魔力
+        public int baseSpeed;   // 速さ
+
+        public ElementType element; // 属性
+    }
+    /// <summary>
+    /// レベル換算後ステータス
+    /// </summary>
+    public class StatusCalculated
+    {
+        public int atk;     // 攻撃力
+        public int def;     // 防御力
+        public int matk;    // 魔法攻撃力
+        public int mdef;    // 魔法防御力
+        public int mp;      // 魔力
+        public int speed;   // 速さ
+    }
+    /// <summary>
+    /// 値を変化させる用のステータス
+    /// </summary>
+    public class StatusRuntime
+    {
         public int atk;
         public int def;
         public int matk;
         public int mdef;
         public int mp;
         public int speed;
-
-        public ElementType element;
     }
 }
