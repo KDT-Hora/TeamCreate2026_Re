985968ac0dc9278a434f829392f86ba9a60ab061
Shaf <KDT24SGC40005@edu.kobe-tech.ac.jp>
2025-12-31 17:27:41 +0900

    コメント追加

--- Assets/Script/DataBase/GameData.cs
+++ Assets/Script/DataBase/GameData.cs
@@ -5,3 +5,9 @@ public static class GameData
     public static int playerCount = 3;
     public static int enemyCount = 3;
 }
+/*
+ * ‚±‚ñ‚ÈŠ´‚¶‚Åg‚¤—\’èH
+    GameData.playerCount = 10;
+    GameData.enemyCount = 5;
+    UnityEngine.SceneManagement.SceneManager.LoadScene("BattleScene");
+ */
