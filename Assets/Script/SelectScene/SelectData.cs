5c54c9f7a96a4ac2acf181f79ab175d4023fb0f3
KDT-Takai <kdt24sgc40005@edu.kobe-tech.ac.jp>
2026-01-24 16:07:30 +0900

    SelectSceneの追加

---
+++ Assets/Script/SelectScene/SelectData.cs
@@ -0,0 +1,41 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class SelectData : MonoBehaviour
+{
+    public static SelectData Instance { get; private set; }
+
+    // ボス
+    public enum Boss
+    {
+        Ittan,
+        Karasu,
+        Syuten
+    }
+    // 選択したボスだけ保持する
+    public Boss selectBoss;
+
+    private void Awake()
+    {
+        if(Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Setter
+    public void SetBoss(Boss boss)
+    {
+        Debug.Log("選択したボス: " + boss);
+        selectBoss = boss;
+    }
+
+    // Getter
+    public Boss GetBoss()
+    {
+        return selectBoss;    
+    }
+}
